eab9cacd74c7f37f1c6cde802b7a4a4f569a7a71
yolandaclbrtarigan08 <yolandaclbrtarigan@gmail.com>
2026-04-08 19:20:51 +0800

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-function showMenu(menu){
-    document.querySelectorAll('.section').forEach(sec=>{
-        sec.classList.remove('active');
-    });
-    document.getElementById(menu).classList.add('active');
-}
+
